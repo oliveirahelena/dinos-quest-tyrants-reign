d723bc917e0e7b587e601e8e2d2b1bfead7c85ac
Helena Oliveira <helena@informach.com.br>
2023-07-16 19:00:44 -0300

    Update pxt.json, main.blocks, main.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,11 @@
 namespace SpriteKind {
     export const Timer = SpriteKind.create()
     export const Sword = SpriteKind.create()
+    export const Boss = SpriteKind.create()
 }
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-    if (!(isAttacking)) {
-        isAttacking = true
+    if (!(sprites.readDataBoolean(Dino, "isAttacking"))) {
+        sprites.setDataBoolean(Dino, "isAttacking", true)
         if (characterAnimations.matchesRule(Dino, characterAnimations.rule(Predicate.FacingRight))) {
             animation.runImageAnimation(
             Dino,
@@ -37,11 +38,10 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
         characterAnimations.setCharacterAnimationsEnabled(Dino, false)
         timer.after(800, function () {
             characterAnimations.setCharacterAnimationsEnabled(Dino, true)
-            isAttacking = false
+            sprites.setDataBoolean(Dino, "isAttacking", false)
         })
     }
 })
-let isAttacking = false
 let Dino: Sprite = null
 tiles.setTilemap(tilemap`level1`)
 Dino = sprites.create(assets.image`Dino-down`, SpriteKind.Player)
@@ -71,14 +71,42 @@ assets.animation`walk-dino-front`,
 200,
 characterAnimations.rule(Predicate.FacingDown, Predicate.Moving)
 )
+let Tyrant = sprites.create(assets.image`tyrant-front`, SpriteKind.Boss)
+characterAnimations.loopFrames(
+Tyrant,
+assets.animation`tyrant-walk-right`,
+200,
+characterAnimations.rule(Predicate.FacingRight, Predicate.Moving)
+)
+characterAnimations.loopFrames(
+Tyrant,
+assets.animation`tyrant-walk-left`,
+200,
+characterAnimations.rule(Predicate.FacingLeft, Predicate.Moving)
+)
+characterAnimations.loopFrames(
+Tyrant,
+assets.animation`walk-dino-back`,
+200,
+characterAnimations.rule(Predicate.FacingUp, Predicate.Moving)
+)
+characterAnimations.loopFrames(
+Tyrant,
+assets.animation`tyrant-walk-front`,
+200,
+characterAnimations.rule(Predicate.FacingDown, Predicate.Moving)
+)
 game.onUpdate(function () {
-    if (characterAnimations.matchesRule(Dino, characterAnimations.rule(Predicate.NotMoving, Predicate.FacingRight)) && !(isAttacking)) {
+    if (characterAnimations.matchesRule(Dino, characterAnimations.rule(Predicate.NotMoving, Predicate.FacingRight)) && !(sprites.readDataBoolean(Dino, "isAttacking"))) {
         Dino.setImage(assets.image`Dino-down-side`)
-    } else if (characterAnimations.matchesRule(Dino, characterAnimations.rule(Predicate.NotMoving, Predicate.FacingLeft)) && !(isAttacking)) {
+    } else if (characterAnimations.matchesRule(Dino, characterAnimations.rule(Predicate.NotMoving, Predicate.FacingLeft)) && !(sprites.readDataBoolean(Dino, "isAttacking"))) {
         Dino.setImage(assets.image`Dino-down-side0`)
-    } else if (characterAnimations.matchesRule(Dino, characterAnimations.rule(Predicate.NotMoving, Predicate.FacingUp)) && !(isAttacking)) {
+    } else if (characterAnimations.matchesRule(Dino, characterAnimations.rule(Predicate.NotMoving, Predicate.FacingUp)) && !(sprites.readDataBoolean(Dino, "isAttacking"))) {
         Dino.setImage(assets.image`Dino-down-back`)
-    } else if (characterAnimations.matchesRule(Dino, characterAnimations.rule(Predicate.NotMoving, Predicate.FacingDown)) && !(isAttacking)) {
+    } else if (characterAnimations.matchesRule(Dino, characterAnimations.rule(Predicate.NotMoving, Predicate.FacingDown)) && !(sprites.readDataBoolean(Dino, "isAttacking"))) {
         Dino.setImage(assets.image`Dino-down`)
     }
+})
+game.onUpdateInterval(1000, function () {
+    Tyrant.setVelocity(randint(-70, 70), randint(-50, 50))
 })
